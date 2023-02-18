03c50540bf1d89edf91f55654c7967db9b851860
Gjorgjevikj <dejan.gjorgjevikj@gmail.com>
2023-02-18 21:34:00 +0100

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine(Environment.OSVersion.VersionString);
+            //string inp = Console.ReadLine();
+            //Console.WriteLine(inp);
+        }
+    }
+}
